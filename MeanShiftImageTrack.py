94c560ffdc0e7d6e79e8b6d48d068236211f6411
Kushnir Dmytro <kushnir.dmytro.93@gmail.com>
2020-11-01 03:32:04 +0200

    Add local notebook for ContentBasedImageRetreaval, update on MeanShift WIP

--- MeanShiftImageTrack.py
+++ MeanShiftImageTrack.py
@@ -61,8 +61,7 @@ def MeanShiftIteration(lkh, values, counts, domain_span, kernel_r, init_pos, ver
 
     return new_mean_point
 
-def DrawRectOnImage(im, top_left, bot_right):
-    color = (0, 255, 0)
+def DrawRectOnImage(im, top_left, bot_right, color=(0, 255, 0)):
     thickness = 2
     im = cv2.rectangle(im, top_left, bot_right, color, thickness)
     return im
@@ -84,11 +83,20 @@ def main():
     # TODO: Read border box for tracking region
     # TODO: Read path for dataset
 
+    # for j in range(0, 3):
+    #     img = np.random.normal(size=(100, 150))
+    #     plt.figure(1);
+    #     plt.clf()
+    #     plt.imshow(img)
+    #     plt.title('Number ' + str(j))
+    #     plt.pause(3)
+
     bb_top_left = [0, 0]
     bb_width = -1
     bb_height = -1
     print(os.getcwd())
-    with open("data/Biker/Biker/groundtruth_rect.txt") as expected_rects:
+    # with open("data/Biker/Biker/groundtruth_rect.txt") as expected_rects:
+    with open("data/Bolt/Bolt/groundtruth_rect.txt") as expected_rects:
         line = expected_rects.readline()
         bboxValue = [ int(val) for val in line.split(',') if len(val) != 0]
         bb_top_left[0] = bboxValue[0]
@@ -96,14 +104,23 @@ def main():
         bb_width = bboxValue[2]
         bb_height = bboxValue[3]
 
+
     dataset = []
-    im_dir_name = "data/Biker/Biker/img"
-    for impath in os.listdir(im_dir_name):
-        print(impath)
+    # im_dir_name = "data/Biker/Biker/img"
+    im_dir_name = "data/Bolt/Bolt/img"
+    for impath in tqdm(os.listdir(im_dir_name)):
         dataset.append(cv2.imread(join(im_dir_name, impath), cv2.IMREAD_COLOR))
+    print("Dataset size:", len(dataset))
+
+    # dataset = [cv2.cvtColor(im, cv2.COLOR) for im in dataset]
 
     im = dataset[0]
 
+    # Constants
+    EPS = np.finfo(np.float32).eps
+    HIST_BINS = 100
+
+    # init stage
     start_point = (bb_top_left[0], bb_top_left[1])
     end_point = (start_point[0] + bb_width, start_point[1] + bb_height)
     color = (0, 255, 0)
@@ -116,82 +133,77 @@ def main():
     plt.imshow(drawn_im)
     plt.show()
 
-    HIST_BINS = 100
     image_histo = NpHisto(im, HIST_BINS)
     image_histo_normalized = image_histo / np.sum(image_histo)
 
     reference_histo = NpHisto(im[bb_top_left[1]:bb_top_left[1]+bb_height, bb_top_left[0]:bb_top_left[0]+bb_width], HIST_BINS)
     reference_histo_normalized = reference_histo / np.sum(reference_histo)
 
-    EPS = np.finfo(np.float32).eps
-
-    likelyhood_im_shape = (im.shape[0] - bb_height, im.shape[1] - bb_width)
-    likelyhood_im = np.zeros(likelyhood_im_shape)
-
-
-    old_center = np.round( (np.array(start_point) + np.array(end_point)) / 2)
+    old_center = np.round((np.array(start_point) + np.array(end_point)) / 2)
     R = np.sqrt(bb_height * bb_height + bb_width + bb_width)
-    c_range = range(int(max(0, old_center[0] - R/2)), int(min(likelyhood_im.shape[0], old_center[0] + R/2)))
-    r_range = range(int(max(0, old_center[1] - R / 2)), int(min(likelyhood_im.shape[1], old_center[1] + R / 2)))
+    for new_im in dataset[1:]:
+        likelyhood_im_shape = (im.shape[0] - bb_height, im.shape[1] - bb_width)
+        likelyhood_im = np.zeros(likelyhood_im_shape)
 
-    # Computing likelihood
-    im = dataset[1]
-    # for row in tqdm(range(likelyhood_im_shape[0])):
-    for row in tqdm(r_range):
-        for col in c_range:
-        # for col in range(likelyhood_im_shape[1]):
-            patch = im[row:row+bb_height, col:col+bb_width]
-            patch_histo = NpHisto(patch, HIST_BINS)
-            histo_sum = np.sum(patch_histo) + EPS
-            patch_histo_normalized = patch_histo / histo_sum
-            histo_diff = reference_histo_normalized - patch_histo_normalized
-            likelyhood = 1 - np.linalg.norm(histo_diff)
-            likelyhood_im[row, col] = likelyhood
+        c_range = range(int(max(0, old_center[0] - R/2)), int(min(likelyhood_im.shape[0], old_center[0] + R/2)))
+        r_range = range(int(max(0, old_center[1] - R / 2)), int(min(likelyhood_im.shape[1], old_center[1] + R / 2)))
 
-    plt.imshow(likelyhood_im)
-    plt.show()
+        # for row in tqdm(range(likelyhood_im_shape[0])):
+        for row in tqdm(r_range):
+            # for col in range(likelyhood_im_shape[1]):
+            for col in c_range:
+                patch = new_im[row:row+bb_height, col:col+bb_width]
+                patch_histo = NpHisto(patch, HIST_BINS)
+                histo_sum = np.sum(patch_histo) + EPS
+                patch_histo_normalized = patch_histo / histo_sum
+                histo_diff = reference_histo_normalized - patch_histo_normalized
+                likelyhood = 1 - np.linalg.norm(histo_diff)
+                likelyhood_im[row, col] = likelyhood
 
-    r_range_list = list(r_range)
-    c_range_list = list(c_range)
-    print(r_range_list[0])
-    print(r_range_list[1])
-    print(c_range_list[0])
-    print(c_range_list[1])
-    plt.imshow(likelyhood_im)
-    plt.show()
-    weights = likelyhood_im[r_range_list[0]:r_range_list[-1], c_range_list[0]:c_range_list[-1]]
-    print("weights shape", weights.shape)
+        # plt.imshow(likelyhood_im)
+        # plt.show()
 
-    # TODO: replace with some build-in function call
-    coords = np.zeros( (likelyhood_im.shape[0], likelyhood_im.shape[1], 3) )
-    for r in r_range:
-        for c in c_range:
-            coords[r, c, 0] = r
-            coords[r, c, 1] = c
+        r_range_list = list(r_range)
+        c_range_list = list(c_range)
+        weights = likelyhood_im[r_range_list[0]:r_range_list[-1], c_range_list[0]:c_range_list[-1]]
 
-    coords = coords[r_range_list[0]:r_range_list[-1], c_range_list[0]:c_range_list[-1], :2]
-    print(weights.shape)
-    print(coords.shape)
-    coords = np.reshape(coords, (coords.shape[0]*coords.shape[1], 2))
-    weights = np.reshape(weights, (weights.shape[0]*weights.shape[1]))
-    print(coords.shape)
-    print(coords[0])
-    # print(coords.reshape((coords.shape[0]*coords.shape[1]), 2))
-    new_mean = np.average(coords,
-                          weights=weights,
-                          axis=0)
-    new_mean = np.round(new_mean).astype(int)
-    print(new_mean.shape)
-    print(new_mean)
-    im_with_track = cv2.circle(im, (new_mean[1], new_mean[0]), 1, [255, 0, 0], 2)
+        # TODO: replace with some build-in function call
+        coords = np.zeros( (likelyhood_im.shape[0], likelyhood_im.shape[1], 3) )
+        for r in r_range:
+            for c in c_range:
+                coords[r, c, 0] = r
+                coords[r, c, 1] = c
 
-    old_center = old_center.astype(int)
-    im_with_track2 = cv2.circle(im_with_track, (old_center[0], old_center[1]), 1, [0, 0, 255], 2)
-    plt.imshow(im_with_track2)
-    plt.show()
+        coords = coords[r_range_list[0]:r_range_list[-1], c_range_list[0]:c_range_list[-1], :2]
+        coords = np.reshape(coords, (coords.shape[0]*coords.shape[1], 2))
+        weights = np.reshape(weights, (weights.shape[0]*weights.shape[1]))
+        new_mean = np.average(coords,
+                              weights=weights,
+                              axis=0)
+        new_mean = np.round(new_mean).astype(int)
 
-    # TODO: Perform tracking
+        print("new mean shape:", new_mean.shape)
+        print("new mean:", new_mean)
 
+        old_center = old_center.astype(int)
+
+        offset = new_mean - [old_center[1], old_center[0]]
+        old_center = np.array([new_mean[1], new_mean[0]])
+        im = new_im
+        drawn_im = DrawRectOnImage(im,
+                                   top_left=(bb_top_left[0], bb_top_left[1]),
+                                   bot_right=(bb_top_left[0] + bb_width, bb_top_left[1] + bb_height),
+                                   color=(0, 0, 255))
+
+        bb_top_left += offset
+        drawn_im = DrawRectOnImage(drawn_im,
+                                   top_left=(bb_top_left[0], bb_top_left[1]),
+                                   bot_right=(bb_top_left[0] + bb_width, bb_top_left[1] + bb_height),
+                                   color=(0, 255, 0))
+        plt.imshow(drawn_im)
+        plt.pause(0.1)
+
+        # TODO: Perform tracking
 
 if __name__ == "__main__":
     main()
